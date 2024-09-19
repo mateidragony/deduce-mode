6d243d4efb673cf78e62fe91eb89ca3b5a0143b9
Matei Cloteaux <matei.cloteaux@gmail.com>
2024-09-19 14:23:08 -0400

    Deduce mode auto-load

--- deduce-mode.el
+++ deduce-mode.el
@@ -2,11 +2,44 @@
 
 ;; Author: Matei Cloteaux <matei.cloteaux@gmail.com>
 ;; Version: 0.0.1
-;; Package-Requires ((emacs "24") (parent-mode "2.0")) 
+;; Package-Requires ((emacs "24") (haskell-mode "1")) 
 
 ;; This file is NOT part of GNU Emacs
 
-;;; Code: 
+;; Copyright (c) 2013-2016, Fanael Linithien
+;; All rights reserved.
+;;
+;; Redistribution and use in source and binary forms, with or without
+;; modification, are permitted provided that the following conditions are
+;; met:
+;;
+;;   * Redistributions of source code must retain the above copyright
+;;     notice, this list of conditions and the following disclaimer.
+;;   * Redistributions in binary form must reproduce the above copyright
+;;     notice, this list of conditions and the following disclaimer in the
+;;     documentation and/or other materials provided with the distribution.
+;;
+;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+;; IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+;; TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+;; PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+;; OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+;; EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+;; PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+;; PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+;; LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+;; NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+;; SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+;;; Commentary:
+
+;; This major mode provides syntax highlighting for deduce programs in emacs
+;;
+;; To enable: call `deduce-mode'.
+
+;;; Code:
+
+(require 'haskell-mode)
 
 (defvar deduce-prims nil "deduce primitives")
 (setq deduce-prims '("0" "true" "false" "∅" "[0]" "?"))
@@ -51,11 +84,10 @@
               (cons dfunctions-regex      'font-lock-function-name-face)
 	      (cons "->\\|++\\|/\\||\\|&\\|\\\[+\\\]\\|\\\[o\\\]\\|(=\\|<=\\|>=\\|/=\\|≠\\|⊆\\|≤\\|<\\|≥\\|∈\\|∪\\|+\\|%\\|*\\|⨄\\|-\\|∩\\|∘\\|>" 'font-lock-constant-face))))
 
+;;;###autoload
 (define-derived-mode deduce-mode haskell-mode "deduce mode"
       "Major mode for editing Deduce languge"
       (setq font-lock-defaults '((deduce-fontlock))))
-
-(add-to-list 'auto-mode-alist '("\\.\\(pf\\|thm\\)\\'" . deduce-mode))
 
 (provide 'deduce-mode)
 
